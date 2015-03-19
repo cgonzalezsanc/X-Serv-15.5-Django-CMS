55dbbeb293a81dfc98526ead89d6b32d2813340e
Carlos Gonzalez Sanchez <carlosgosa94@gmail.com>
2015-03-19 11:41:19 +0100

    Ejercicio 15.5

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import Pages
 
 # Create your views here.
+
+def handler(request, recurso):
+    fila = Pages.objects.filter(name=recurso) 
+    if len(fila) == 0:
+        if recurso == "":
+            fila = Pages(name=recurso, page="Pagina principal")
+        else:
+            fila = Pages(name=recurso, page="Pagina de " + recurso)
+        fila.save()
+        return HttpResponse(fila.page)
+    else:
+        return HttpResponse(fila[0].page)
